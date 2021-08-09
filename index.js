6e0d33dbd5f138117cbb7f2beb85f227c26db62a
Bishwajit Jha <ajitjha393@gmail.com>
2021-08-09 20:52:11 +0530

    feat(vayu):some spooky stuff

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 import { Vayu } from './vayu'
 
+const Weather = (dt, temp) => ({ dt, temp })
+
+const toFahrenheit = (k) => (k - 273.15) * 1.8 + 32
+
+const toWeather = (dt, temp) =>
+  Weather(new Date(dt).toLocaleDateString(), toFahrenheit(temp))
+
+const prepareItems = (w) => toWeather(w.dt, w.main.temp)
+
+const getWeatherItems = (zip) =>
+  Vayu.getWeather(zip)
+    .map((json) => json.list.map(prepareItems))
+    .map((weather) => weather.map(toLi))
+
+const toLi = (weather) => `<li>${weather.dt} ${weather.temp}</li>`
+
 //=========== IMPURE CODE :) =============//
 
 const app = () => {
@@ -9,7 +25,9 @@ const app = () => {
 
   goButton.addEventListener('click', () => {
     const zipCode = input.value.trim()
-    Vayu.getWeather(zipCode).fork(console.error, console.log)
+    getWeatherItems(zipCode).fork(console.error, (html) => {
+      results.innerHTML = html
+    })
   })
 }
 

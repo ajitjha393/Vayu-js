b8861cf760b33d6485bed16ba480d3724cd8641d
Bishwajit Jha <ajitjha393@gmail.com>
2021-08-09 19:41:28 +0530

    feat(app): separate pure with impure code

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+import { Task } from './functional-types'
 
-const zip = 400069  
-const fetchIt = url => fetch(`http://api.openweathermap.org/data/2.5/forecast?zip=${zip},in&APPID=${process.env.WEATHER_API_KEY}`
-)
+const getOpenWeather = (zip) =>
+  `http://api.openweathermap.org/data/2.5/forecast?zi)p=${zip},in&APPID=${process.env.WEATHER_API_KEY}`
 
-fetchIt()
-.then(res => res.json())
-.then(console.log)
+const fetchIt = (url) =>
+  Task((reject, resolve) => fetch(url).then(resolve).catch(reject))
+
+const zip = 400069
+
+fetchIt(getOpenWeather(zip))
+
+//=========== IMPURE CODE :) =============//
+
+const app = () => {
+  const goButton = document.getElementById('go')
+  const input = document.getElementById('zip')
+  const results = document.getElementById('results')
+
+  goButton.addEventListener('click', () => {
+    const zipCode = input.value.trim()
+    fetchIt(getOpenWeather(zipCode)).fork(console.error, console.log)
+  })
+}
